935c0fd54192f4a2c8b3d311b078c96702e88131
Ratiug-99 <evgeniy.basiuk@gmail.com>
2021-06-27 23:18:36 +0300

    create ApiClient.kt

---
+++ app/src/main/java/com/ratiugdev/weatherapp/data/api/ApiClient.kt
@@ -0,0 +1,34 @@
+package com.ratiugdev.weatherapp.data.api
+
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiClient {
+    companion object {
+        private const val TAG = "DBG | ApiClient"
+        private const val BASE_URL = "https://www.metaweather.com/api/"
+    }
+
+    private val gson: Gson by lazy {
+        Log.d(TAG, "create gson")
+        GsonBuilder().setLenient().create()
+    }
+
+    private val okHttpClient: OkHttpClient by lazy {
+        Log.d(TAG, "create okHttpClient")
+        OkHttpClient.Builder().build()
+    }
+
+    private val retrofit : Retrofit by lazy {
+        Log.d(TAG, "create retrofit")
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+}
