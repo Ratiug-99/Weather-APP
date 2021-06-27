bd9dffcf6cef728bdab541074faffe40f8ac5eb8
Ratiug-99 <evgeniy.basiuk@gmail.com>
2021-06-27 22:39:46 +0300

    set basic dependency

--- app/src/main/java/com/ratiugdev/weatherapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/ratiugdev/weatherapp/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ratiugdev.weatherapp
+package com.ratiugdev.weatherapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ratiugdev.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
