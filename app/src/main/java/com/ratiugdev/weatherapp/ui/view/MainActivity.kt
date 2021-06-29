846d307a4aa1645aaea73ae640ef9a162b2bdd4d
Ratiug-99 <evgeniy.basiuk@gmail.com>
2021-06-29 20:52:53 +0300

    create search by name interface

--- app/src/main/java/com/ratiugdev/weatherapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/ratiugdev/weatherapp/ui/view/MainActivity.kt
@@ -2,7 +2,13 @@ package com.ratiugdev.weatherapp.ui.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.ratiugdev.weatherapp.R
@@ -22,13 +28,30 @@ class MainActivity : AppCompatActivity() {
 //    private lateinit var resultSearchAdapter: ResultSearchAdapter
 
     private lateinit var recyclerViewResultSearch: RecyclerView
+    private lateinit var etSearchLocation: EditText
+    private lateinit var pbLoadingResult: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupViewModel()
         setupUI()
+        setupListeners()
         setupObserver()
+    }
+
+
+    private fun setupUI() {
+        recyclerViewResultSearch = findViewById(R.id.rv_result_search)
+        etSearchLocation = findViewById(R.id.et_search_location)
+        pbLoadingResult = findViewById(R.id.pb_loading_search_location)
+
+    }
+
+    private fun setupListeners() {
+        etSearchLocation.doAfterTextChanged {
+            mainViewModel.searchLocationByName(it.toString())
+        }
     }
 
     private fun setupViewModel() {
@@ -40,60 +63,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupObserver() {
 
-        mainViewModel.searchLocationByName("kiev").observe(this, {
+        mainViewModel.listResultSearch.observe(this, {
             it?.let { resource ->
                 when (resource.status) {
                     LOADING -> {
+                        pbLoadingResult.visibility = VISIBLE
+                        recyclerViewResultSearch.visibility = GONE
                         Log.d(TAG, "setupObserver searchLocationByName LOADING")
                     }
                     SUCCESS -> {
-                        Log.d(TAG, "setupObserver searchLocationByName SUCCESS")
+                        pbLoadingResult.visibility = GONE
+                        recyclerViewResultSearch.visibility = VISIBLE
+
+                        Log.d(TAG, "setupObserver searchLocationByName SUCCESS \n ${it.data}")
                     }
                     ERROR -> {
+                        pbLoadingResult.visibility = GONE
+                        recyclerViewResultSearch.visibility = GONE
                         Log.d(TAG, "setupObserver searchLocationByName ERROR - ${it.message}")
                     }
                 }
             }
         })
 
-//
-//        mainViewModel.searchLocationByCoordinate("50.068,-5.316").observe(this, {
-//            it?.let { resource ->
-//                when (resource.status) {
-//                    LOADING -> {
-//                        Log.d(TAG, "setupObserver  searchLocationByName LOADING")
-//                    }
-//                    SUCCESS -> {
-//                        Log.d(TAG, "setupObserver searchLocationByName SUCCESS")
-//                    }
-//                    ERROR -> {
-//                        Log.d(TAG, "setupObserver searchLocationByName ERROR - ${it.message}")
-//                    }
-//                }
-//            }
-//        })
 
-//        mainViewModel.getLocation(44418).observe(this, {
-//            it?.let { resource ->
-//                when (resource.status) {
-//                    LOADING -> {
-//                        Log.d(TAG, "setupObserver  getLocation LOADING")
-//                    }
-//                    SUCCESS -> {
-//                        Log.d(TAG, "setupObserver getLocation SUCCESS")
-//                    }
-//                    ERROR -> {
-//                        Log.d(TAG, "setupObserver getLocation ERROR - ${it.message}")
-//                    }
-//                }
-//            }
-//        })
-
-
-    }
-
-    private fun setupUI() {
-        recyclerViewResultSearch = findViewById(R.id.rv_result_search)
     }
 
 
--- app/src/main/java/com/ratiugdev/weatherapp/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ratiugdev/weatherapp/ui/viewmodel/MainViewModel.kt
@@ -1,22 +1,38 @@
 package com.ratiugdev.weatherapp.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import com.ratiugdev.weatherapp.data.model.Location
 import com.ratiugdev.weatherapp.data.repository.MainRepository
 import com.ratiugdev.weatherapp.utils.Resource
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
+    val listResultSearch = MutableLiveData<Resource<List<Location>?>>()
 
-
-    fun searchLocationByName(locationName: String) = liveData(Dispatchers.IO){
-        emit(Resource.loading(null))
-        try {
-            emit(Resource.success(mainRepository.searchLocationsByName(locationName)))
-        } catch (exception : Exception) {
-            emit(Resource.error(null,exception.message ?: "Something went wrong"))
+    fun searchLocationByName(locationName: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            listResultSearch.postValue(Resource.loading(null))
+            try {
+                listResultSearch.postValue(Resource.success(mainRepository.searchLocationsByName(locationName)))
+            } catch (exception: Exception){
+                listResultSearch.postValue(Resource.error(null,exception.message ?: "Something went wrong"))
+            }
         }
+
     }
+
+//    fun searchLocationByName(locationName: String) = liveData(Dispatchers.IO){
+//        emit(Resource.loading(null))
+//        try {
+//            emit(Resource.success(mainRepository.searchLocationsByName(locationName)))
+//        } catch (exception : Exception) {
+//            emit(Resource.error(null,exception.message ?: "Something went wrong"))
+//        }
+//    }
 
     fun searchLocationByCoordinate(locationName: String) = liveData(Dispatchers.IO){
         emit(Resource.loading(null))
